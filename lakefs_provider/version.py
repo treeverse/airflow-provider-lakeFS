09f173f7c4ae15f5b233499eb9ed852a36a9a7a0
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2023-08-15 09:28:46 +0300

    [bug] importlib cannot retrieve version before packaging

--- lakefs_provider/version.py
+++ lakefs_provider/version.py
@@ -7,5 +7,17 @@ if sys.version_info >= (3, 8):
 else:
     import importlib_metadata as metadata
 
+
+def __get_version(package: str) -> str:
+    try:
+        return metadata.version(package)
+    except metadata.PackageNotFoundError as e:
+        print("No package metadata found")
+        return None
+    except Exception as e:
+        print(f"Get version: {e}")
+        return None
+
+
 # Extract version using method #5 of https://packaging.python.org/en/latest/guides/single-sourcing-package-version/#single-sourcing-the-version
-__version__ = metadata.version('airflow-provider-lakefs')
+__version__ = __get_version('airflow-provider-lakefs')
