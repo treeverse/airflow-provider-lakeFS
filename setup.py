033f327bd847a576dd9bb1409389dd0ac7a3b24b
Barak Amar <barak.amar@treeverse.io>
2023-10-16 15:40:28 +0300

    bump lakefs sdk version to v0.113.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     packages=['lakefs_provider', 'lakefs_provider.hooks', 'lakefs_provider.links',
               'lakefs_provider.sensors', 'lakefs_provider.operators',
               'lakefs_provider.example_dags'],
-    install_requires=['apache-airflow>=2.0', 'lakefs_client>=0.112.1'],
+    install_requires=['apache-airflow>=2.0', 'lakefs_client>=0.113.0'],
     setup_requires=['setuptools', 'wheel'],
     author='Treeverse',
     author_email='services@treeverse.io',
